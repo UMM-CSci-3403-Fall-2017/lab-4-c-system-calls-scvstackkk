b447d6f7b5cb43e5c59b7df2674646a8d3e0acad
scvstackkk <scv.stack@gmail.com>
2017-10-30 16:47:17 -0500

    Finished writing summarize_tree.c, but need to debug an error occuring with calls to opendir() -> returning a null pointer.

--- summarize_tree/summarize_tree.c
+++ summarize_tree/summarize_tree.c
@@ -8,14 +8,28 @@
 
 static int num_dirs, num_regular;
 
+/*
+ * Use the stat() function (try "man 2 stat") to determine if the file
+ * referenced by path is a directory or not.  Call stat, and then use
+ * S_ISDIR to see if the file is a directory. Make sure you check the
+ * return value from stat in case there is a problem, e.g., maybe the
+ * the file doesn't actually exist.
+ */
 bool is_dir(const char* path) {
-  /*
-   * Use the stat() function (try "man 2 stat") to determine if the file
-   * referenced by path is a directory or not.  Call stat, and then use
-   * S_ISDIR to see if the file is a directory. Make sure you check the
-   * return value from stat in case there is a problem, e.g., maybe the
-   * the file doesn't actually exist.
-   */
+    struct stat buf;
+    bool is_dir;
+    int errcode;
+    
+    errcode = stat(path, &buf);
+
+    if (errcode != 0) {
+       printf("stat() returned with error code %d.\n", errcode);
+       return 1;
+    }
+    else {
+      is_dir = S_ISDIR(buf.st_mode);
+      return is_dir;
+    }
 }
 
 /* 
@@ -24,24 +38,57 @@ bool is_dir(const char* path) {
  */
 void process_path(const char*);
 
+/*
+ * Update the number of directories seen, use opendir() to open the
+ * directory, and then use readdir() to loop through the entries
+ * and process them. You have to be careful not to process the
+ * "." and ".." directory entries, or you'll end up spinning in
+ * (infinite) loops. Also make sure you closedir() when you're done.
+ *
+ * You'll also want to use chdir() to move into this new directory,
+ * with a matching call to chdir() to move back out of it when you're
+ * done.
+ */
 void process_directory(const char* path) {
-  /*
-   * Update the number of directories seen, use opendir() to open the
-   * directory, and then use readdir() to loop through the entries
-   * and process them. You have to be careful not to process the
-   * "." and ".." directory entries, or you'll end up spinning in
-   * (infinite) loops. Also make sure you closedir() when you're done.
-   *
-   * You'll also want to use chdir() to move into this new directory,
-   * with a matching call to chdir() to move back out of it when you're
-   * done.
-   */
+    DIR* current_dir;
+    struct dirent* ds;
+    int errcode;
+    
+    if ((errcode = chdir(path)) != 0) {
+        printf("chdir() returned with error code %d while moving to %s.\n", errcode, path);
+    	return;
+    }
+    else {	
+	if ((current_dir = opendir(path)) == NULL) {
+	    printf("opendir() returned a null pointer while opening %s.\n", path);
+	    return;
+	}	    
+
+    	while ((ds = readdir(current_dir)) != NULL) {
+    	   if (strcmp(ds->d_name, ".") != 0 && strcmp(ds->d_name, "..") != 0) {
+    	       process_path(ds->d_name);
+	   } 
+	}
+
+	if ((errcode = closedir(current_dir)) != 0) {
+	    printf("closedir() returned with error code %d.\n", errcode);
+	    return;
+	}
+
+	if ((errcode = chdir("..")) != 0) {
+	    printf("chdir() returned with error code %d while moving out of %s.\n", errcode, path);
+	    return;
+	}
+        
+	++num_dirs;
+    }    
 }
 
+/*
+ * Update the number of regular files.
+ */
 void process_file(const char* path) {
-  /*
-   * Update the number of regular files.
-   */
+    ++num_regular; 
 }
 
 void process_path(const char* path) {
